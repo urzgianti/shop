2a0d9761c0a0ed7ad8f7fa5885ef902ca1b52b43
urzgianti <kmaleckaite@gmail.com>
2016-11-24 00:12:25 +0200

    pakeisti def

--- shop/shopdb/models.py
+++ shop/shopdb/models.py
@@ -14,7 +14,6 @@ class ItemGroupCatalog(models.Model):
 
 class ProductAttribute(models.Model):
     itemgroupcatalog = models.ForeignKey(ItemGroupCatalog, on_delete=models.CASCADE)
-
     name = models.CharField(max_length=200)
     size = models.CharField(max_length=200)
     price = models.CharField(max_length=200)
@@ -25,14 +24,14 @@ class ProductAttribute(models.Model):
 class Client(models.Model):
     fname = models.CharField(max_length=200)
     lname = models.CharField(max_length=200)
-    def __str__(self):
-        return "%s %s" % (self.fname, self.lname)
-
     gender = models.CharField(max_length=6, choices=GENDER)
     bdate = models.DateField()
     telephone = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s %s" % (self.fname, self.lname)
 
 class Card(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -44,64 +43,34 @@ class Card(models.Model):
 
 class Shop(models.Model):
     shopname = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    shopphone = models.CharField(max_length=200)
+
     def __str__(self):
         return self.shopname
 
-    address = models.CharField(max_length=200)
-    def __str__(self):
-        return self.address
-
-    shopphone = models.CharField(max_length=200)
-    def __str__(self):
-        return self.shopphone
-
 class Employee(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.shop
-
     efname = models.CharField(max_length=200)
-    def __str__(self):
-        return self.efname
-
     elname = models.CharField(max_length=200)
-    def __str__(self):
-        return self.elname
-
     egender = models.CharField(max_length=6, choices=GENDER)
-    def __str__(self):
-        return self.egender
-
     ebdate = models.DateField()
-    def __str__(self):
-        return self.ebdate
-
     ephone = models.CharField(max_length=200)
-    def __str__(self):
-        return self.ephone
-
     eemail = models.CharField(max_length=200)
+
     def __str__(self):
-        return self.eemail
+        return "%s %s" % (self.efname, self.elname)
+
 
 class Order(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.shop
-
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.client
-
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.employee
-
     collection_date = models.DateField()
-    def __str__(self):
-        return self.collection_date
+    def __int__(self):
+        return self.id
 
 class Basket(models.Model):
     productattribute = models.ManyToManyField(ProductAttribute)
-    def __str__(self):
-        return self.basket
+    def __int__(self):
+        return self.id
